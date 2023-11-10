018313a5c1576be59da3f2ea58ebfd21bbcd843e
Jesus Briones <leojesusbriones2@gmail.com>
2023-11-10 11:01:32 -0500

    avances2

---
+++ code/snnipets/input-console.js
@@ -0,0 +1,15 @@
+import readline from 'node:readline'
+
+
+// Crea la interfaz del prompt
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+})
+
+
+// Pide datos en una iteración.
+rl.question('Escribe: ', (input) => {
+  console.log(`Tu escribiste esto: ${input}`)
+  rl.close()
+})

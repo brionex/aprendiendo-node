0ae49dbfdffb87851f31180042b658e192a7696f
Jesus Briones <leojesusbriones2@gmail.com>
2025-01-15 12:28:05 -0500

    change file extension to .cjs

--- src/aliases.js
+++ src/aliases.js
@@ -1,6 +1,6 @@
 export const aliases = {
   globalThis: './codigo/1.variable-globalThis/main.js',
-  commonJs: './codigo/2.sistema-de-modulos/cjs/main.js',
+  commonJs: './codigo/2.sistema-de-modulos/cjs/main.cjs',
   esModules: './codigo/2.sistema-de-modulos/mjs/main.js',
   inputConsole: './snnipets/input-console.js',
 }

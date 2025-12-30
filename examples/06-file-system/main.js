2c84d51400e1d3f2de011f4734f1526fe96f8396
brionex <leojesusbriones2@gmail.com>
2025-12-30 13:00:22 -0500

    update

--- examples/06-file-system/main.js
+++ examples/06-file-system/main.js
@@ -2,11 +2,9 @@ import { fsSync } from './1-fs-sync.js'
 import { fsAsync } from './2-fs-async.js'
 import { stat } from './3-stat.js'
 
-// File system síncrono
-fsSync()
+const arg = process.argv[2]
 
-// File system asíncrono
-// fsAsync()
-
-// File system stat
-// stat()
+if (arg === '1') fsSync()
+else if (arg === '2') fsAsync()
+else if (arg === '3') stat()
+else fsSync()
